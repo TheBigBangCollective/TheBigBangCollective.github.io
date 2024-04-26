a62ba9099d8a1cc8fb3b216920de4b14174db0d3
Kaj Siebert <kaj@k-si.com>
2024-04-26 13:10:06 +0200

    Make mouse pointer radioactive

--- src/util/bubble.js
+++ src/util/bubble.js
@@ -2,16 +2,23 @@
 
 class Particle {
     constructor(x, y) {
+        const speedFactor = 5;
+
         this.x = x;
         this.y = y;
-        this.velocityX = Math.random() * 5;
-        this.velocityY = Math.random() * 5;
+        this.velocityX = speedFactor/2 - Math.random() * speedFactor;
+        this.velocityY = speedFactor/2 - Math.random() * speedFactor;
         this.mass = Math.random() * 3;
         this.charge = Math.floor(Math.random() * 2);
         this.isCharged = this.charge !== 0;
         this.energyLossFactor = 0.995; // Factor to simulate energy loss per second
         this.life = Math.random() * 500;
+        this.startLife = this.life;
         this.lastTime = Date.now();
+
+        if (this.isCharged && Math.random() < 0.5) {
+            this.charge = -1;
+        }
     }
 
     update(magneticField) {
@@ -49,20 +56,26 @@ class Particle {
             this.update(magneticField);
             const [x1, y1] = [this.x, this.y];
 
-            ctx.beginPath();
-            ctx.moveTo(x0, y0);
-            ctx.lineTo(x1, y1);
-            ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)';
-            ctx.stroke();
+            if (this.startLife - this.life > 3) {
+                ctx.beginPath();
+                ctx.moveTo(x0, y0);
+                ctx.lineTo(x1, y1);
+                ctx.strokeStyle = 'rgba(0, 0, 0, 0.2)';
+                ctx.lineWidth = 3;
+                ctx.stroke();
+            }
         }
     }
 }
 
 
 export function init(canvas) {
-    const particles = []
+    let isMouseOverWindow = false;
+    let mouseX = 0;
+    let mouseY = 0;
     const ctx = canvas.getContext('2d');
 
+    // Make sure canvas fills window
     function resizeCanvas() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
@@ -70,6 +83,23 @@ export function init(canvas) {
     
     window.addEventListener('resize', resizeCanvas);
     resizeCanvas(); 
+    
+
+    // Event listener for mouse entering the window
+    window.document.addEventListener('mouseenter', function() {
+        isMouseOverWindow = true;
+    });
+
+    // Event listener for mouse leaving the window
+    window.document.addEventListener('mouseleave', function() {
+        isMouseOverWindow = false;
+    });
+
+    // Listen to mousemove event
+    window.addEventListener('mousemove', function(event) {
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+    });
 
     let lastDraw = 0;
 
@@ -85,20 +115,23 @@ export function init(canvas) {
             lastDraw = Date.now();
         }
 
+        const particles = []
+
         // Add random particles
-        if (Math.random() < 0.1) {
+        if (Math.random() < 0.025) {
             particles.push(new Particle(
                 Math.random() * canvas.width,
                 Math.random() * canvas.height,
             ));
         }
 
+        // If mouse over window
+        if (isMouseOverWindow && Math.random() < 0.1) {
+            particles.push(new Particle(mouseX, mouseY));
+        }
 
         particles.forEach((particle, index) => {
             particle.draw(ctx, 0.005);
-            if (particle.life < 0) {
-                particles.splice(index, 1);
-            }
         });
 
         requestAnimationFrame(draw);
