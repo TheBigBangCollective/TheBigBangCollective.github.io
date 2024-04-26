cd3a1c4390fbc42ff42416f2c986654deb5ff8a6
Kaj Siebert <kaj@k-si.com>
2024-04-26 13:18:09 +0200

    Move scroll-header to separate file

---
+++ src/util/scroll-header.js
@@ -0,0 +1,13 @@
+export function initScrollHeader() {
+    window.addEventListener('scroll', function() {
+		const header = document.querySelector('.header-fixed');
+		const title = document.querySelector('.bb-title');
+		if (window.scrollY > 10) {
+			header.classList.add('header-scrolled');
+			title.classList.add('bb-title-scrolled');
+		} else {
+			header.classList.remove('header-scrolled');
+			title.classList.remove('bb-title-scrolled');
+		}
+	});
+}
