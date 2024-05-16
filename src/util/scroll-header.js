994f036680bade076326d49f51a2f3fa1c4daed3
Kaj Siebert <kaj@k-si.com>
2024-05-16 12:58:26 +0100

    Add more content on front-page

--- src/util/scroll-header.js
+++ src/util/scroll-header.js
@@ -1,13 +1,16 @@
+export function doScroll() {
+	const header = document.querySelector('.header-fixed');
+	const title = document.querySelector('.bb-title');
+	if (window.scrollY > 10) {
+		header.classList.add('header-scrolled');
+		title.classList.add('bb-title-scrolled');
+	} else {
+		header.classList.remove('header-scrolled');
+		title.classList.remove('bb-title-scrolled');
+	}
+}
+
 export function initScrollHeader() {
-    window.addEventListener('scroll', function() {
-		const header = document.querySelector('.header-fixed');
-		const title = document.querySelector('.bb-title');
-		if (window.scrollY > 10) {
-			header.classList.add('header-scrolled');
-			title.classList.add('bb-title-scrolled');
-		} else {
-			header.classList.remove('header-scrolled');
-			title.classList.remove('bb-title-scrolled');
-		}
-	});
+    window.addEventListener('scroll', doScroll);
+	doScroll();
 }
